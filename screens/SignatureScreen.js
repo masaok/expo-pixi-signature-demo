648bb7cab1f2ef1006002ec309696c919bcd1c17
Masao Kitamura <masaok@gmail.com>
2019-07-03 22:04:07 -0700

    Adding title to Signature screen

--- screens/SignatureScreen.js
+++ screens/SignatureScreen.js
@@ -66,7 +66,7 @@ export default class SignatureScreen extends React.Component {
 }
 
 SignatureScreen.navigationOptions = {
-  header: null,
+  title: 'Signature Demo',
 };
 
 const styles = StyleSheet.create({
